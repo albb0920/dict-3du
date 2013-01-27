12d2eaed3901096f977b3acbee20655056d11822
Albert Song <albb0920@gmail.com>
2013-01-27 11:22:53 +0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dict3du::Application.config.secret_token = 'd1603b93511117bdbecd0412085a5f540e9a566a71c53b27cd43fdcfe9734318d85b282f3caa730e85274236578eab7bae4fbe9f640ef81907ae86b7eeab8271'

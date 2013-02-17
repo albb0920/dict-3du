6011f7b01e0608f43c9a9c5dd49bfd1f3700dc38
Albert Song <albb0920@gmail.com>
2013-02-17 16:18:48 +0800

    Make idx of definitions work.

--- app/models/definition.rb
+++ app/models/definition.rb
@@ -1,9 +1,11 @@
 class Definition < ActiveRecord::Base
   self.inheritance_column = nil
-  attr_accessible :def, :example, :type, :source, :synonyms, :antonyms, :heteronym_id
+  attr_accessible :def, :example, :type, :source, :synonyms, :antonyms, :heteronym_id, :idx
 
   has_comma_splited_fields :synonyms, :antonyms
 
   belongs_to :heteronym
   belongs_to :link, polymorphic: true
+
+  default_scope order('idx ASC')
 end
